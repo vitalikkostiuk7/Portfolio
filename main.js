4ebe3a95bba0b19950b2c79eb3d176b129dfd6ba
vitalikkostiuk7 <kostukvitalik98@gmail.com>
2023-04-27 01:18:04 +0300

    delete snow

--- main.js
+++ main.js
@@ -136,51 +136,51 @@ btnHamburger.addEventListener('click', function () {
 
 // Snow start
 
-function SnowFalls(ob){
-    this.param={count:75, color:['blue'], minSize: 10, maxSize:30, letter:'*', speed:1};
-    for(a in ob)
-        if(a in this.param)this.param[a]=ob[a];
-    this.param.color=[...this.param.color];
-    this.param.letter=[...this.param.letter];
-    this.width=Math.max(document.body.clientWidth, innerWidth);
-    this.height=Math.max(document.body.clientHeight, innerHeight);
-    this.el=[];
-    for(var i=0;i<this.param.count;i++){
-      this.el[i]=document.createElement('div');
-      this.el[i].innerHTML=this.param.letter[parseInt(Math.random()*this.param.letter.length)];
-      this.el[i].style.position='fixed';
-      this.el[i].style.top='-50px';
-      this.el[i].style.left=0
-      this.el[i].top=0-(Math.random()*this.height);
-      this.el[i].crds=0;
-      this.el[i].left_radius=Math.random()*(15-5)+5;
-      this.el[i].left_sped=0.00001+(Math.random()/55);
-      this.el[i].left_x=parseInt(Math.random()*this.width);
-      this.el[i].left=0;
-      this.el[i].style.color=this.param.color[parseInt(this.param.color.length*Math.random())];
-      this.el[i].style.fontSize=parseInt(Math.random()*(this.param.maxSize-this.param.minSize)+this.param.minSize)+'px';
-      this.el[i].style.opacity=Math.random();
-      this.el[i].style.zIndex=1000;
-      document.body.appendChild(this.el[i]);
-    }
-    this.animation=function(){
-      for(var i=0, l=this.el.length; i<l;i++){
-      this.el[i].crds+=this.el[i].left_sped;
-      this.el[i].left=this.el[i].left_x+this.el[i].left_radius*Math.sin(this.el[i].crds);
-      this.el[i].top+=this.param.speed;
-      if(this.el[i].top>this.height){this.el[i].top=-50;
-      this.el[i].left=parseInt(Math.random()*this.width);
-      }
-      this.el[i].style.top=this.el[i].top+'px';
-      this.el[i].style.left=this.el[i].left+'px';
-      this.el[i].left=Math.random()*this.width;
-      }
-      requestAnimationFrame(this.animation.bind(this));
+// function SnowFalls(ob){
+//     this.param={count:75, color:['blue'], minSize: 10, maxSize:30, letter:'*', speed:1};
+//     for(a in ob)
+//         if(a in this.param)this.param[a]=ob[a];
+//     this.param.color=[...this.param.color];
+//     this.param.letter=[...this.param.letter];
+//     this.width=Math.max(document.body.clientWidth, innerWidth);
+//     this.height=Math.max(document.body.clientHeight, innerHeight);
+//     this.el=[];
+//     for(var i=0;i<this.param.count;i++){
+//       this.el[i]=document.createElement('div');
+//       this.el[i].innerHTML=this.param.letter[parseInt(Math.random()*this.param.letter.length)];
+//       this.el[i].style.position='fixed';
+//       this.el[i].style.top='-50px';
+//       this.el[i].style.left=0
+//       this.el[i].top=0-(Math.random()*this.height);
+//       this.el[i].crds=0;
+//       this.el[i].left_radius=Math.random()*(15-5)+5;
+//       this.el[i].left_sped=0.00001+(Math.random()/55);
+//       this.el[i].left_x=parseInt(Math.random()*this.width);
+//       this.el[i].left=0;
+//       this.el[i].style.color=this.param.color[parseInt(this.param.color.length*Math.random())];
+//       this.el[i].style.fontSize=parseInt(Math.random()*(this.param.maxSize-this.param.minSize)+this.param.minSize)+'px';
+//       this.el[i].style.opacity=Math.random();
+//       this.el[i].style.zIndex=1000;
+//       document.body.appendChild(this.el[i]);
+//     }
+//     this.animation=function(){
+//       for(var i=0, l=this.el.length; i<l;i++){
+//       this.el[i].crds+=this.el[i].left_sped;
+//       this.el[i].left=this.el[i].left_x+this.el[i].left_radius*Math.sin(this.el[i].crds);
+//       this.el[i].top+=this.param.speed;
+//       if(this.el[i].top>this.height){this.el[i].top=-50;
+//       this.el[i].left=parseInt(Math.random()*this.width);
+//       }
+//       this.el[i].style.top=this.el[i].top+'px';
+//       this.el[i].style.left=this.el[i].left+'px';
+//       this.el[i].left=Math.random()*this.width;
+//       }
+//       requestAnimationFrame(this.animation.bind(this));
       
-  }
-    this.animation();
-  }
+//   }
+//     this.animation();
+//   }
   
-  new SnowFalls({color:['#9CC7D5']});
+//   new SnowFalls({color:['#9CC7D5']});
 
 // Snow end
